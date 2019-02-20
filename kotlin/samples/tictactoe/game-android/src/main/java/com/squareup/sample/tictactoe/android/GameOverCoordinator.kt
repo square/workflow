0d9736072f8522cf6a3ba75ebc158f2ba3c0342c
Ray Ryan <ray@squareup.com>
2019-02-19 16:04:03 -0800

    Revert "Use stock Toolbar in sample."
    This reverts commit fbe28b84c10489d0ed8e672877a18a47e86851f4.

--- kotlin/samples/tictactoe/game-android/src/main/java/com/squareup/sample/tictactoe/android/GameOverCoordinator.kt
+++ kotlin/samples/tictactoe/game-android/src/main/java/com/squareup/sample/tictactoe/android/GameOverCoordinator.kt
@@ -15,10 +15,10 @@
  */
 package com.squareup.sample.tictactoe.android
 
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
+import android.widget.Button
+import android.widget.TextView
 import com.squareup.coordinators.Coordinator
 import com.squareup.sample.tictactoe.CompletedGame
 import com.squareup.sample.tictactoe.Ending.Draw
@@ -45,23 +45,20 @@ internal class GameOverCoordinator(
   private val subs = CompositeDisposable()
 
   private lateinit var board: ViewGroup
-  private lateinit var toolbar: Toolbar
-  private lateinit var saveItem: MenuItem
-  private lateinit var exitItem: MenuItem
+  private lateinit var banner: TextView
+  private lateinit var button1: Button
+  private lateinit var button2: Button
 
   override fun attach(view: View) {
     super.attach(view)
 
     board = view.findViewById(R.id.board_view)
-    toolbar = view.findViewById(R.id.toolbar)
+    banner = view.findViewById(R.id.game_banner)
+    button1 = view.findViewById(R.id.game_banner_button_1)
+    button2 = view.findViewById(R.id.game_banner_button_2)
 
-    with(toolbar.menu) {
-      saveItem = add("")
-      exitItem = add(R.string.exit)
-    }
-
-    saveItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-    exitItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+    button2.visibility = View.VISIBLE
+    button2.text = view.resources.getText(R.string.exit)
 
     subs.add(screens.subscribe { s -> update(view, s) })
   }
@@ -75,42 +72,43 @@ internal class GameOverCoordinator(
     view: View,
     screen: GameOverScreen
   ) {
-    exitItem.setOnMenuItemClickListener {
-      screen.onEvent(NoMore)
-      true
-    }
+    button2.setOnClickListener { screen.onEvent(NoMore) }
     // Start a new game when they hit the back button.
     view.setBackHandler { screen.onEvent(PlayAgain) }
 
     when (screen.data.syncState) {
       SAVING -> {
-        saveItem.isEnabled = false
-        saveItem.title = "…saving…"
-        saveItem.setOnMenuItemClickListener(null)
+        button1.isEnabled = false
+        button1.text = "…saving…"
+        button1.setOnClickListener(null)
       }
       SAVE_FAILED -> {
-        saveItem.isEnabled = true
-        saveItem.title = "Unsaved"
-        saveItem.setOnMenuItemClickListener {
-          screen.onEvent(TrySaveAgain)
-          true
-        }
+        button1.isEnabled = true
+        button1.text = "Unsaved"
+        button1.setOnClickListener { screen.onEvent(TrySaveAgain) }
       }
       SAVED -> {
-        saveItem.isVisible = false
-        saveItem.setOnMenuItemClickListener(null)
+        button1.visibility = View.GONE
+        button1.setOnClickListener(null)
       }
     }
 
-    renderGame(screen.data.completedGame)
+    renderGame(board, banner, screen.data.completedGame)
   }
 
-  private fun renderGame(completedGame: CompletedGame) {
-    renderResult(completedGame)
+  private fun renderGame(
+    board: ViewGroup,
+    banner: TextView,
+    completedGame: CompletedGame
+  ) {
+    renderResult(banner, completedGame)
     renderBoard(board, completedGame.lastTurn.board)
   }
 
-  private fun renderResult(completedGame: CompletedGame) {
+  private fun renderResult(
+    textView: TextView,
+    completedGame: CompletedGame
+  ) {
     val player = completedGame.lastTurn.playing
     val playerName = completedGame.lastTurn.players[player]
 
@@ -120,7 +118,7 @@ internal class GameOverCoordinator(
       Quitted -> "$playerName ($player) is a quitter!"
     }
 
-    toolbar.title = message
+    textView.text = message
   }
 
   /** Note how easily  we're sharing this layout with [GamePlayCoordinator]. */
--- kotlin/samples/tictactoe/game-android/src/main/java/com/squareup/sample/tictactoe/android/GamePlayCoordinator.kt
+++ kotlin/samples/tictactoe/game-android/src/main/java/com/squareup/sample/tictactoe/android/GamePlayCoordinator.kt
@@ -18,7 +18,6 @@ package com.squareup.sample.tictactoe.android
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toolbar
 import com.squareup.coordinators.Coordinator
 import com.squareup.sample.tictactoe.Board
 import com.squareup.sample.tictactoe.GamePlayScreen
@@ -38,13 +37,17 @@ internal class GamePlayCoordinator(
   private val subs = CompositeDisposable()
 
   private lateinit var board: ViewGroup
-  private lateinit var toolbar: Toolbar
+  private lateinit var banner: TextView
 
   override fun attach(view: View) {
     super.attach(view)
 
     board = view.findViewById(R.id.board_view)
-    toolbar = view.findViewById(R.id.toolbar)
+    banner = view.findViewById(R.id.game_banner)
+    view.findViewById<View>(R.id.game_banner_button_1)
+        .visibility = View.INVISIBLE
+    view.findViewById<View>(R.id.game_banner_button_2)
+        .visibility = View.INVISIBLE
 
     subs.add(screens.subscribe { update(view, it) })
   }
@@ -58,7 +61,7 @@ internal class GamePlayCoordinator(
     view: View,
     screen: GamePlayScreen
   ) {
-    renderPlayer(screen.data)
+    renderPlayer(banner, screen.data)
     renderBoard(board, screen.data.board)
 
     setCellClickListeners(board, screen.data, screen.onEvent)
@@ -85,11 +88,14 @@ internal class GamePlayCoordinator(
     }
   }
 
-  private fun renderPlayer(turn: Turn) {
+  private fun renderPlayer(
+    banner: TextView,
+    turn: Turn
+  ) {
     val yourTurn = turn.players[turn.playing]
     val mark = turn.playing.name
     val message = String.format("%s, place your %s", yourTurn, mark)
-    toolbar.title = message
+    banner.text = message
   }
 
   companion object : ViewBinding<GamePlayScreen> by LayoutBinding.of(
