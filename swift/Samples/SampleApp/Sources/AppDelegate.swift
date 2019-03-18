dc5f16e2727b787d058800fd38c67a360a3a4412
Tim Donnelly <tim@tdonnelly.com>
2019-03-18 11:10:08 -0700

    Add Empty Swift sample app (and move Cocoapods tests to use the integrated sample app) (#186)

---
+++ swift/Samples/SampleApp/Sources/AppDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
