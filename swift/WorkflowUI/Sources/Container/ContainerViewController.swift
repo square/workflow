2de5e98c24fdac2e4e8b3aa79c0d4949acd3cafe
Tim Donnelly <tim@tdonnelly.com>
2019-03-19 11:46:36 -0700

    Add WorkflowUI (#189)
    * Add WorkflowUI
    
    * Add WorkflowUI to Travis config

---
+++ swift/WorkflowUI/Sources/Container/ContainerViewController.swift
@@ -0,0 +1,100 @@
+import UIKit
+import ReactiveSwift
+import Result
+import Workflow
+
+
+/// Drives view controllers from a root Workflow.
+public final class ContainerViewController<Output>: UIViewController {
+
+    /// Emits output events from the bound workflow.
+    public let output: Signal<Output, NoError>
+
+    internal let renderer: UIKitRenderableViewController
+
+    private let workflowHost: Any
+
+    private let rendering: Property<Screen>
+
+    private let (lifetime, token) = Lifetime.make()
+
+    private init(workflowHost: Any, rendering: Property<Screen>, output: Signal<Output, NoError>, viewRegistry: ViewRegistry) {
+        self.workflowHost = workflowHost
+        self.renderer = UIKitRenderableViewController(viewRegistry: viewRegistry)
+        self.rendering = rendering
+        self.output = output
+
+        super.init(nibName: nil, bundle: nil)
+
+        rendering
+            .signal
+            .take(during: lifetime)
+            .observeValues { [weak self] screen in
+                self?.render(screen: screen)
+            }
+
+        render(screen: rendering.value)
+    }
+
+    public convenience init<W: Workflow>(workflow: W, viewRegistry: ViewRegistry) where W.Rendering == Screen, W.Output == Output {
+        let host = WorkflowHost(workflow: workflow)
+        self.init(
+            workflowHost: host,
+            rendering: host.rendering,
+            output: host.output,
+            viewRegistry: viewRegistry)
+    }
+
+    public convenience init<W: Workflow>(workflow: W, viewRegistry: ViewRegistry) where W.Rendering: Screen, W.Output == Output {
+        let host = WorkflowHost(workflow: workflow)
+        self.init(
+            workflowHost: host,
+            rendering: host.rendering.map { $0 as Screen },
+            output: host.output,
+            viewRegistry: viewRegistry)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func render(screen: Screen) {
+        renderer.render(screen: screen)
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        addChild(renderer)
+        view.addSubview(renderer.view)
+        renderer.didMove(toParent: self)
+    }
+
+    override public func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        renderer.view.frame = view.bounds
+    }
+
+    public override var childForStatusBarStyle: UIViewController? {
+        return renderer
+    }
+
+    public override var childForStatusBarHidden: UIViewController? {
+        return renderer
+    }
+
+    public override var childForHomeIndicatorAutoHidden: UIViewController? {
+        return renderer
+    }
+
+    public override var childForScreenEdgesDeferringSystemGestures: UIViewController? {
+        return renderer
+    }
+
+    public override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return renderer.supportedInterfaceOrientations
+    }
+
+}
---
+++ swift/WorkflowUI/Tests/ViewRegistryTests.swift
@@ -0,0 +1,60 @@
+import XCTest
+
+@testable import WorkflowUI
+
+
+fileprivate struct ScreenA: Screen {}
+fileprivate struct ScreenB: Screen {}
+
+fileprivate class ViewA: ScreenViewController<ScreenA> {}
+fileprivate class ViewB: ScreenViewController<ScreenB> {}
+fileprivate class ViewC: ScreenViewController<ScreenA> {}
+
+class ViewRegistryTests: XCTestCase {
+
+    func test_can_provides_for_registered_views() {
+        var viewRegistry = ViewRegistry()
+        viewRegistry.register(screenViewControllerType: ViewA.self)
+
+        XCTAssertFalse(viewRegistry.canProvideView(for: ScreenB.self))
+        XCTAssertTrue(viewRegistry.canProvideView(for: ScreenA.self))
+    }
+
+    func test_provides_for_registered_views() {
+        var viewRegistry = ViewRegistry()
+        viewRegistry.register(screenViewControllerType: ViewA.self)
+
+        XCTAssertTrue(viewRegistry.canProvideView(for: ScreenA.self))
+        let actual = viewRegistry.provideView(for: ScreenA())
+
+        XCTAssertTrue(type(of: actual) == ViewA.self)
+    }
+
+    func test_merge_joins_registries() {
+        var viewRegistry1 = ViewRegistry()
+        var viewRegistry2 = ViewRegistry()
+
+        viewRegistry1.register(screenViewControllerType: ViewA.self)
+        viewRegistry2.register(screenViewControllerType: ViewB.self)
+
+        viewRegistry1.merge(with: viewRegistry2)
+        XCTAssertTrue(viewRegistry1.canProvideView(for: ScreenA.self))
+        XCTAssertTrue(viewRegistry1.canProvideView(for: ScreenB.self))
+    }
+
+    func test_merge_other_registry_wins_for_duplicates() {
+        var viewRegistry1 = ViewRegistry()
+        var viewRegistry2 = ViewRegistry()
+
+        viewRegistry1.register(screenViewControllerType: ViewA.self)
+
+
+        viewRegistry2.register(screenViewControllerType: ViewC.self)
+
+
+        viewRegistry1.merge(with: viewRegistry2)
+        XCTAssertTrue(viewRegistry1.canProvideView(for: ScreenA.self))
+        let actual = viewRegistry1.provideView(for: ScreenA())
+        XCTAssertTrue(type(of: actual) == ViewC.self)
+    }
+}
