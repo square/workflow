a35b4257abc4ad9136788a11735a2a352cea6330
Dave Apgar <git+apgar@squareup.com>
2019-04-11 10:21:55 -0700

    Updated Workflow template compose declaration to return an explicit Screen type.
    A compose method can no longer return `Screen`, but must return a typed screen.
    Updates the template to reflect this.
    
    Resolves #270

--- swift/Tooling/Templates/Workflow (Verbose).xctemplate/___FILEBASENAME___Workflow.swift
+++ swift/Tooling/Templates/Workflow (Verbose).xctemplate/___FILEBASENAME___Workflow.swift
@@ -79,8 +79,8 @@ extension ___VARIABLE_productName___Workflow {
 
 extension ___VARIABLE_productName___Workflow {
 
-    func compose(state: ___VARIABLE_productName___Workflow.State, context: WorkflowContext<___VARIABLE_productName___Workflow>) -> Screen {
-        #warning("Don't forget your compose implementation!")
-        fatalError()
+    func compose(state: ___VARIABLE_productName___Workflow.State, context: WorkflowContext<___VARIABLE_productName___Workflow>) -> String {
+        #warning("Don't forget your compose implementation and to return the correct rendering type!")
+        return "This is likely not the rendering that you want to return"
     }
 }
