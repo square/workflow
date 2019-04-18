square/workflow

commit cf3ae7bfa5f1d3778d057d5d200ee4126ba4cf58

Add the ability to update the input for a WorkflowHost.
Closes #247.

kotlin/workflow-runtime/src/main/java/com/squareup/workflow/WorkflowHost.kt
@@ -19,24 +19,29 @@ package com.squareup.workflow
 
 import com.squareup.workflow.WorkflowHost.Factory
 import com.squareup.workflow.WorkflowHost.Update
-import com.squareup.workflow.internal.WorkflowId
 import com.squareup.workflow.internal.WorkflowNode
 import com.squareup.workflow.internal.id
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.NonCancellable.isActive
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.selects.select
 import org.jetbrains.annotations.TestOnly
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.coroutineContext
 
 /**
  * Provides a stream of [updates][Update] from a tree of [Workflow]s.
  *
  * Create these by injecting a [Factory] and calling [run][Factory.run].
  */
-interface WorkflowHost<out OutputT : Any, out RenderingT : Any> {
+interface WorkflowHost<in InputT : Any, out OutputT : Any, out RenderingT : Any> {
 
   /**
    * Output from a [WorkflowHost]. Emitted from [WorkflowHost.updates] after every compose pass.
@@ -62,88 +67,134 @@ interface WorkflowHost<out OutputT : Any, out RenderingT : Any> {
     /**
      * Creates a [WorkflowHost] to run [workflow].
      *
-     * The workflow's initial state is determined by passing [input] to
-     * [StatefulWorkflow.initialState].
+     * The workflow's initial state is determined by passing the first value emitted by [inputs] to
+     * [StatefulWorkflow.initialState]. Subsequent values emitted from [inputs] will be used to
+     * re-render the workflow.
      *
      * @param workflow The workflow to start.
-     * @param input Passed to [StatefulWorkflow.initialState] to determine the root workflow's
-     * initial state. If [InputT] is `Unit`, you can just omit this argument.
+     * @param inputs Passed to [StatefulWorkflow.initialState] to determine the root workflow's
+     * initial state, and to pass input updates to the root workflow.
+     * If [InputT] is `Unit`, you can just omit this argument.
      * @param snapshot If not null, used to restore the workflow.
      * @param context The [CoroutineContext] used to run the workflow tree. Added to the [Factory]'s
      * context.
      */
     fun <InputT : Any, OutputT : Any, RenderingT : Any> run(
       workflow: Workflow<InputT, OutputT, RenderingT>,
-      input: InputT,
+      inputs: ReceiveChannel<InputT>,
       snapshot: Snapshot? = null,
       context: CoroutineContext = EmptyCoroutineContext
-    ): WorkflowHost<OutputT, RenderingT> = run(workflow.id(), workflow, input, snapshot, context)
+    ): WorkflowHost<InputT, OutputT, RenderingT> =
+      object : WorkflowHost<InputT, OutputT, RenderingT> {
+        private val scope = CoroutineScope(context)
+
+        override val updates: ReceiveChannel<Update<OutputT, RenderingT>> =
+          scope.produce(capacity = 0) {
+            runWorkflowTree(
+                workflow = workflow.asStatefulWorkflow(),
+                inputs = inputs,
+                initialSnapshot = snapshot,
+                onUpdate = ::send
+            )
+          }
+      }
 
     fun <OutputT : Any, RenderingT : Any> run(
       workflow: Workflow<Unit, OutputT, RenderingT>,
       snapshot: Snapshot? = null,
       context: CoroutineContext = EmptyCoroutineContext
-    ): WorkflowHost<OutputT, RenderingT> = run(workflow.id(), workflow, Unit, snapshot, context)
+    ): WorkflowHost<Unit, OutputT, RenderingT> = run(workflow, channelOf(Unit), snapshot, context)
 
     /**
      * Creates a [WorkflowHost] that runs [workflow] starting from [initialState].
      *
      * **Don't call this directly.**
      *
-     * Instead, your module should have a test dependency on `pure-v2-testing` and you should call the
-     * testing extension method defined there on your workflow itself.
+     * Instead, your module should have a test dependency on `pure-v2-testing` and you should call
+     * the testing extension method defined there on your workflow itself.
      */
     @TestOnly
     fun <InputT : Any, StateT : Any, OutputT : Any, RenderingT : Any> runTestFromState(
       workflow: StatefulWorkflow<InputT, StateT, OutputT, RenderingT>,
       input: InputT,
       initialState: StateT
-    ): WorkflowHost<OutputT, RenderingT> {
-      val workflowId = workflow.id()
-      return object : WorkflowHost<OutputT, RenderingT> {
-        val node = WorkflowNode(workflowId, workflow, input, null, baseContext, initialState)
+    ): WorkflowHost<InputT, OutputT, RenderingT> =
+      object : WorkflowHost<InputT, OutputT, RenderingT> {
         override val updates: ReceiveChannel<Update<OutputT, RenderingT>> =
-          node.start(workflow, input)
+          GlobalScope.produce(capacity = 0, context = baseContext) {
+            runWorkflowTree(
+                workflow = workflow.asStatefulWorkflow(),
+                inputs = channelOf(input),
+                initialSnapshot = null,
+                initialState = initialState,
+                onUpdate = ::send
+            )
+          }
       }
-    }
 
-    internal fun <InputT : Any, OutputT : Any, RenderingT : Any> run(
-      id: WorkflowId<InputT, OutputT, RenderingT>,
-      workflow: Workflow<InputT, OutputT, RenderingT>,
-      input: InputT,
-      snapshot: Snapshot?,
-      context: CoroutineContext
-    ): WorkflowHost<OutputT, RenderingT> = object : WorkflowHost<OutputT, RenderingT> {
-      val node = WorkflowNode(
-          id = id,
-          workflow = workflow.asStatefulWorkflow(),
-          initialInput = input,
-          snapshot = snapshot,
-          baseContext = baseContext + context
-      )
-      override val updates: ReceiveChannel<Update<OutputT, RenderingT>> =
-        node.start(workflow.asStatefulWorkflow(), input)
-    }
+    private fun <T> channelOf(value: T) = Channel<T>(capacity = 1)
+        .apply { offer(value) }
   }
 }
 
 /**
- * Starts the coroutine that runs the coroutine loop.
+ * Loops forever, or until the coroutine is cancelled, processing the workflow tree and emitting
+ * updates by calling [onUpdate].
+ *
+ * This function is the lowest-level entry point into the runtime. Don't call this directly, instead
+ * use [WorkflowHost.Factory] to create a [WorkflowHost], or one of the stream operators for your
+ * favorite Rx library to map a stream of [InputT]s into [Update]s.
  */
-internal fun <I : Any, O : Any, R : Any> WorkflowNode<I, *, O, R>.start(
-  workflow: StatefulWorkflow<I, *, O, R>,
-  input: I
-): ReceiveChannel<Update<O, R>> = produce(capacity = 0) {
+@UseExperimental(InternalCoroutinesApi::class)
+suspend fun <InputT : Any, StateT : Any, OutputT : Any, RenderingT : Any> runWorkflowTree(
+  workflow: StatefulWorkflow<InputT, StateT, OutputT, RenderingT>,
+  inputs: ReceiveChannel<InputT>,
+  initialSnapshot: Snapshot?,
+  initialState: StateT? = null,
+  onUpdate: suspend (Update<OutputT, RenderingT>) -> Unit
+): Nothing {
+  var output: OutputT? = null
+  var input: InputT = inputs.receive()
+  var inputsClosed = false
+  val rootNode = WorkflowNode(
+      id = workflow.id(),
+      workflow = workflow,
+      initialInput = input,
+      snapshot = initialSnapshot,
+      baseContext = coroutineContext,
+      initialState = initialState
+  )
+
   try {
-    var output: O? = null
-    while (isActive) {
-      val rendering = compose(workflow, input)
-      val snapshot = snapshot(workflow)
-      send(Update(rendering, snapshot, output))
+    while (true) {
+      // Manually implement `ensureActive()` until we're on coroutines 1.2.x.
+      if (!isActive) throw coroutineContext[Job]!!.getCancellationException()
+
+      val rendering = rootNode.compose(workflow, input)
+      val snapshot = rootNode.snapshot(workflow)
+
+      onUpdate(Update(rendering, snapshot, output))
+
       // Tick _might_ return an output, but if it returns null, it means the state or a child
       // probably changed, so we should re-compose/snapshot and emit again.
       output = select {
-        tick(this) { it }
+        // Stop trying to read from the inputs channel after it's closed.
+        if (!inputsClosed) {
+          @Suppress("EXPERIMENTAL_API_USAGE")
+          inputs.onReceiveOrNull { newInput ->
+            if (newInput == null) {
+              inputsClosed = true
+            } else {
+              input = newInput
+            }
+            // No output. Returning from the select will go to the top of the loop to do another
+            // compose pass.
+            return@onReceiveOrNull null
+          }
+        }
+
+        // Tick the workflow tree.
+        rootNode.tick(this) { it }
       }
     }
   } catch (e: Throwable) {
@@ -154,9 +205,9 @@ internal fun <I : Any, O : Any, R : Any> WorkflowNode<I, *, O, R>.start(
     coroutineContext.cancel(e)
     throw e
   } finally {
-    // There's a potential race condition if the producer coroutine is cancelled before it has a chance
-    // to enter the try block, since we can't use CoroutineStart.ATOMIC. However, until we actually
-    // see this cause problems, I'm not too worried about it.
-    cancel()
+    // There's a potential race condition if the producer coroutine is cancelled before it has a
+    // chance to enter the try block, since we can't use CoroutineStart.ATOMIC. However, until we
+    // actually see this cause problems, I'm not too worried about it.
+    rootNode.cancel()
   }
 }

kotlin/samples/tictactoe/common/src/test/java/com/squareup/sample/mainworkflow/MainWorkflowTest.kt
@@ -41,6 +41,9 @@ class MainWorkflowTest {
 
     MainWorkflow(authWorkflow, runGameWorkflow()).testFromStart { tester ->
       tester.withNextRendering { screen ->
+        assertThat(screen.panels).containsOnly(DEFAULT_AUTH)
+      }
+      tester.withNextRendering { screen ->
         assertThat(screen.panels).isEmpty()
         assertThat(screen.body).isEqualTo(DEFAULT_RUN_GAME)
       }

kotlin/workflow-rx2/src/test/java/com/squareup/workflow/rx2/SubscriptionsTest.kt
@@ -33,7 +33,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertFalse
-import kotlin.test.assertSame
+import kotlin.test.assertTrue
 
 class SubscriptionsTest {
 
@@ -102,10 +102,12 @@ class SubscriptionsTest {
         setSubscribed(false)
       }
 
-      assertEquals(1, workflow.subscriptions)
-      // For some reason the observable is actually disposed twice. Seems like a coroutines bug, but
+      host.withNextRendering {
+        assertEquals(1, workflow.subscriptions)
+        // For some reason the observable is actually disposed twice. Seems like a coroutines bug, but
       // Disposable.dispose() is an idempotent operation so it should be fine.
       assertEquals(2, workflow.disposals)
+      }
     }
   }
 
@@ -117,10 +119,12 @@ class SubscriptionsTest {
         setSubscribed(false)
       }
 
-      assertEquals(1, workflow.subscriptions)
-      // For some reason the observable is actually disposed twice. Seems like a coroutines bug, but
+      host.withNextRendering {
+        assertEquals(1, workflow.subscriptions)
+        // For some reason the observable is actually disposed twice. Seems like a coroutines bug, but
       // Disposable.dispose() is an idempotent operation so it should be fine.
       assertEquals(2, workflow.disposals)
+      }
     }
   }
 
@@ -165,6 +169,8 @@ class SubscriptionsTest {
 
   @Test fun `observable reports emissions`() {
     workflow.testFromStart(true) { host ->
+      // Get the next rendering to unblock the pipeline so the subscription can actually occur.
+      host.awaitNextRendering()
       assertFalse(host.hasOutput)
 
       subject.onNext("hello")
@@ -179,6 +185,8 @@ class SubscriptionsTest {
 
   @Test fun `observable reports close`() {
     workflow.testFromStart(true) { host ->
+      // Get the next rendering to unblock the pipeline so the subscription can actually occur.
+      host.awaitNextRendering()
       assertFalse(host.hasOutput)
 
       subject.onComplete()
@@ -194,6 +202,8 @@ class SubscriptionsTest {
 
   @Test fun `observable reports close after emission`() {
     workflow.testFromStart(true) { host ->
+      // Get the next rendering to unblock the pipeline so the subscription can actually occur.
+      host.awaitNextRendering()
       assertFalse(host.hasOutput)
 
       subject.onNext("foo")
@@ -206,13 +216,14 @@ class SubscriptionsTest {
   }
 
   @Test fun `observable reports error`() {
-    assertFailsWith<IOException> {
-      workflow.testFromStart(true) { host ->
-        assertFalse(host.hasOutput)
+    workflow.testFromStart(true) { host ->
+      assertFalse(host.hasOutput)
 
-        subject.onError(IOException("fail"))
-        assertSame(Closed, host.awaitNextOutput())
-        assertFalse(host.hasOutput)
+      subject.onError(IOException("fail"))
+
+      host.withFailure { error ->
+        val causeChain = generateSequence(error) { it.cause }
+        assertTrue(causeChain.any { it is IOException })
       }
     }
   }

kotlin/workflow-rx2/src/test/java/com/squareup/workflow/rx2/WorkflowContextsTest.kt
@@ -27,7 +27,9 @@ import com.squareup.workflow.invoke
 import com.squareup.workflow.stateless
 import com.squareup.workflow.testing.testFromStart
 import io.reactivex.subjects.SingleSubject
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
@@ -36,41 +38,42 @@ import kotlin.test.assertTrue
 class WorkflowContextsTest {
 
   @Test fun `onSuccess handles value when emitted during listen`() {
-    var subscriptions = 0
-    var disposals = 0
+    val subscribed = CompletableDeferred<Unit>()
+    val disposed = CompletableDeferred<Unit>()
     val singleSubject = SingleSubject.create<String>()
     val single = singleSubject
-        .doOnSubscribe { subscriptions++ }
-        .doOnDispose { disposals++ }
+        .doOnSubscribe { subscribed.complete(Unit) }
+        .doOnDispose { disposed.complete(Unit) }
     val workflow = Workflow.stateless<String, Unit> { context ->
       context.onSuccess(single) { emitOutput(it) }
     }
 
-    assertEquals(0, subscriptions)
-    assertEquals(0, disposals)
+    assertFalse(subscribed.isCompleted)
+    assertFalse(disposed.isCompleted)
 
     workflow.testFromStart { host ->
-      assertEquals(1, subscriptions)
-      assertEquals(0, disposals)
+      host.awaitNextRendering()
+      runBlocking { subscribed.await() }
+      assertFalse(disposed.isCompleted)
       assertFalse(host.hasOutput)
 
       singleSubject.onSuccess("done!")
 
+      host.awaitNextRendering()
       assertTrue(host.hasOutput)
       assertEquals("done!", host.awaitNextOutput())
-      assertEquals(1, subscriptions)
-      assertEquals(0, disposals)
+      assertFalse(disposed.isCompleted)
     }
   }
 
   @Test fun `onSuccess unsubscribes`() {
-    var subscriptions = 0
-    var disposals = 0
+    val subscribed = CompletableDeferred<Unit>()
+    val disposed = CompletableDeferred<Unit>()
     lateinit var doClose: EventHandler<Unit>
     val singleSubject = SingleSubject.create<Unit>()
     val single = singleSubject
-        .doOnSubscribe { subscriptions++ }
-        .doOnDispose { disposals++ }
+        .doOnSubscribe { subscribed.complete(Unit) }
+        .doOnDispose { disposed.complete(Unit) }
     val workflow = object : StatefulWorkflow<Unit, Boolean, Nothing, Unit>() {
       override fun initialState(
         input: Unit,
@@ -92,18 +95,19 @@ class WorkflowContextsTest {
       override fun snapshotState(state: Boolean): Snapshot = Snapshot.EMPTY
     }
 
-    assertEquals(0, subscriptions)
-    assertEquals(0, disposals)
+    assertFalse(subscribed.isCompleted)
+    assertFalse(disposed.isCompleted)
 
     workflow.testFromStart { host ->
-      assertEquals(1, subscriptions)
-      assertEquals(0, disposals)
+      host.awaitNextRendering()
+      runBlocking { subscribed.await() }
+      assertFalse(disposed.isCompleted)
       assertFalse(host.hasOutput)
 
       doClose()
 
-      assertEquals(1, subscriptions)
-      assertEquals(1, disposals)
+      host.awaitNextRendering()
+      runBlocking { disposed.await() }
     }
   }
 }

kotlin/workflow-testing/src/main/java/com/squareup/workflow/testing/WorkflowTesting.kt
@@ -26,6 +26,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.Channel
 import org.jetbrains.annotations.TestOnly
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
@@ -42,8 +43,11 @@ fun <T, InputT : Any, OutputT : Any, RenderingT : Any>
       input: InputT,
       snapshot: Snapshot? = null,
       context: CoroutineContext = EmptyCoroutineContext,
-      block: (WorkflowTester<OutputT, RenderingT>) -> T
-    ): T = test(block, context) { it.run(this, input, snapshot) }
+      block: (WorkflowTester<InputT, OutputT, RenderingT>) -> T
+    ): T = test(block, context) {
+      val inputs = Channel<InputT>(1).apply { offer(input) }
+      it.run(this, inputs, snapshot)
+    }
 // @formatter:on
 
 /**
@@ -55,7 +59,7 @@ fun <T, InputT : Any, OutputT : Any, RenderingT : Any>
 fun <T, OutputT : Any, RenderingT : Any> Workflow<Unit, OutputT, RenderingT>.testFromStart(
   snapshot: Snapshot? = null,
   context: CoroutineContext = EmptyCoroutineContext,
-  block: (WorkflowTester<OutputT, RenderingT>) -> T
+  block: (WorkflowTester<Unit, OutputT, RenderingT>) -> T
 ): T = testFromStart(Unit, snapshot, context, block)
 
 /**
@@ -72,7 +76,7 @@ fun <T, InputT : Any, StateT : Any, OutputT : Any, RenderingT : Any>
       input: InputT,
       initialState: StateT,
       context: CoroutineContext = EmptyCoroutineContext,
-      block: (WorkflowTester<OutputT, RenderingT>) -> T
+      block: (WorkflowTester<InputT, OutputT, RenderingT>) -> T
     ): T = test(block, context) { it.runTestFromState(this, input, initialState) }
 // @formatter:on
 
@@ -89,15 +93,15 @@ fun <StateT : Any, OutputT : Any, RenderingT : Any>
     StatefulWorkflow<Unit, StateT, OutputT, RenderingT>.testFromState(
       initialState: StateT,
       context: CoroutineContext = EmptyCoroutineContext,
-      block: (WorkflowTester<OutputT, RenderingT>) -> Unit
+      block: (WorkflowTester<Unit, OutputT, RenderingT>) -> Unit
     ) = testFromState(Unit, initialState, context, block)
 // @formatter:on
 
 @UseExperimental(InternalCoroutinesApi::class)
-private fun <T, O : Any, R : Any> test(
-  testBlock: (WorkflowTester<O, R>) -> T,
+private fun <T, I : Any, O : Any, R : Any> test(
+  testBlock: (WorkflowTester<I, O, R>) -> T,
   baseContext: CoroutineContext,
-  starter: (Factory) -> WorkflowHost<O, R>
+  starter: (Factory) -> WorkflowHost<I, O, R>
 ): T {
   val context = Dispatchers.Unconfined + baseContext + Job(parent = baseContext[Job])
   val host = WorkflowHost.Factory(context)

kotlin/workflow-testing/src/test/java/com/squareup/workflow/ChannelSubscriptionsIntegrationTest.kt
@@ -107,8 +107,10 @@ class ChannelSubscriptionsIntegrationTest {
         setSubscribed(false)
       }
 
-      assertEquals(1, workflow.subscriptions)
-      assertEquals(1, workflow.cancellations)
+      host.withNextRendering {
+        assertEquals(1, workflow.subscriptions)
+        assertEquals(1, workflow.cancellations)
+      }
     }
   }
 
@@ -120,8 +122,10 @@ class ChannelSubscriptionsIntegrationTest {
         setSubscribed(false)
       }
 
-      assertEquals(1, workflow.subscriptions)
-      assertEquals(1, workflow.cancellations)
+      host.withNextRendering {
+        assertEquals(1, workflow.subscriptions)
+        assertEquals(1, workflow.cancellations)
+      }
     }
   }
 

kotlin/samples/tictactoe/common/src/test/java/com/squareup/sample/gameworkflow/TakeTurnsWorkflowTest.kt
@@ -69,6 +69,6 @@ class TakeTurnsWorkflowTest {
   }
 }
 
-private fun WorkflowTester<*, GamePlayScreen>.takeSquare(event: TakeSquare) {
+private fun WorkflowTester<*, *, GamePlayScreen>.takeSquare(event: TakeSquare) {
   withNextRendering { it.onEvent(event) }
 }

kotlin/settings.gradle
@@ -23,6 +23,7 @@ include ':samples:tictactoe:common'
 include ':workflow-core'
 include ':workflow-runtime'
 include ':workflow-rx2'
+include ':workflow-rx2-runtime'
 include ':workflow-testing'
 include ':workflow-ui-core'
 include ':workflow-ui-android'

commit a0d9fd6c3425541b7898efc86836e1a990d20f21

Fix for WorkflowHost.Factory not using baseContext.
Commit cf3ae7bfa5f1d3778d057d5d200ee4126ba4cf58 accidentally stopped including
`baseContext` in the context for the workflow loop coroutine. This caused everything
not passing a dispatcher into `run` directly to default to the `Default` dispatcher,
which made a lot of things suddenly happen a lot lazier, and forced us to add a bunch
of additional calls in unit tests.

This PR adds `baseContext` back in, and also adds a default `CoroutineName` element
to aid in debugging.

kotlin/workflow-runtime/src/main/java/com/squareup/workflow/WorkflowHost.kt
@@ -21,6 +21,7 @@ import com.squareup.workflow.WorkflowHost.Factory
 import com.squareup.workflow.WorkflowHost.Update
 import com.squareup.workflow.internal.WorkflowNode
 import com.squareup.workflow.internal.id
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.InternalCoroutinesApi
@@ -35,6 +36,8 @@ import org.jetbrains.annotations.TestOnly
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.coroutineContext
+
+private val DEFAULT_WORKFLOW_COROUTINE_NAME = CoroutineName("WorkflowHost")
 
 /**
  * Provides a stream of [updates][Update] from a tree of [Workflow]s.
@@ -86,7 +89,8 @@ interface WorkflowHost<in InputT : Any, out OutputT : Any, out RenderingT : Any>
       context: CoroutineContext = EmptyCoroutineContext
     ): WorkflowHost<InputT, OutputT, RenderingT> =
       object : WorkflowHost<InputT, OutputT, RenderingT> {
-        private val scope = CoroutineScope(context)
+        // Put the coroutine name first so the passed-in contexts can override it.
+        private val scope = CoroutineScope(DEFAULT_WORKFLOW_COROUTINE_NAME + baseContext + context)
 
         override val updates: ReceiveChannel<Update<OutputT, RenderingT>> =
           scope.produce(capacity = 0) {
@@ -121,7 +125,10 @@ interface WorkflowHost<in InputT : Any, out OutputT : Any, out RenderingT : Any>
     ): WorkflowHost<InputT, OutputT, RenderingT> =
       object : WorkflowHost<InputT, OutputT, RenderingT> {
         override val updates: ReceiveChannel<Update<OutputT, RenderingT>> =
-          GlobalScope.produce(capacity = 0, context = baseContext) {
+          GlobalScope.produce(
+              capacity = 0,
+              context = DEFAULT_WORKFLOW_COROUTINE_NAME + baseContext
+          ) {
             runWorkflowTree(
                 workflow = workflow.asStatefulWorkflow(),
                 inputs = channelOf(input),

kotlin/samples/tictactoe/common/src/test/java/com/squareup/sample/mainworkflow/MainWorkflowTest.kt
@@ -41,9 +41,6 @@ class MainWorkflowTest {
 
     MainWorkflow(authWorkflow, runGameWorkflow()).testFromStart { tester ->
       tester.withNextRendering { screen ->
-        assertThat(screen.panels).containsOnly(DEFAULT_AUTH)
-      }
-      tester.withNextRendering { screen ->
         assertThat(screen.panels).isEmpty()
         assertThat(screen.body).isEqualTo(DEFAULT_RUN_GAME)
       }

kotlin/workflow-rx2/src/test/java/com/squareup/workflow/rx2/SubscriptionsTest.kt
@@ -169,8 +169,6 @@ class SubscriptionsTest {
 
   @Test fun `observable reports emissions`() {
     workflow.testFromStart(true) { host ->
-      // Get the next rendering to unblock the pipeline so the subscription can actually occur.
-      host.awaitNextRendering()
       assertFalse(host.hasOutput)
 
       subject.onNext("hello")
@@ -185,8 +183,6 @@ class SubscriptionsTest {
 
   @Test fun `observable reports close`() {
     workflow.testFromStart(true) { host ->
-      // Get the next rendering to unblock the pipeline so the subscription can actually occur.
-      host.awaitNextRendering()
       assertFalse(host.hasOutput)
 
       subject.onComplete()
@@ -202,8 +198,6 @@ class SubscriptionsTest {
 
   @Test fun `observable reports close after emission`() {
     workflow.testFromStart(true) { host ->
-      // Get the next rendering to unblock the pipeline so the subscription can actually occur.
-      host.awaitNextRendering()
       assertFalse(host.hasOutput)
 
       subject.onNext("foo")

kotlin/workflow-rx2/src/test/java/com/squareup/workflow/rx2/WorkflowContextsTest.kt
@@ -52,14 +52,12 @@ class WorkflowContextsTest {
     assertFalse(disposed.isCompleted)
 
     workflow.testFromStart { host ->
-      host.awaitNextRendering()
       runBlocking { subscribed.await() }
       assertFalse(disposed.isCompleted)
       assertFalse(host.hasOutput)
 
       singleSubject.onSuccess("done!")
 
-      host.awaitNextRendering()
       assertTrue(host.hasOutput)
       assertEquals("done!", host.awaitNextOutput())
       assertFalse(disposed.isCompleted)
@@ -99,14 +97,12 @@ class WorkflowContextsTest {
     assertFalse(disposed.isCompleted)
 
     workflow.testFromStart { host ->
-      host.awaitNextRendering()
       runBlocking { subscribed.await() }
       assertFalse(disposed.isCompleted)
       assertFalse(host.hasOutput)
 
       doClose()
 
-      host.awaitNextRendering()
       runBlocking { disposed.await() }
     }
   }

kotlin/workflow-testing/src/main/java/com/squareup/workflow/testing/WorkflowTesting.kt
@@ -21,7 +21,6 @@ import com.squareup.workflow.Snapshot
 import com.squareup.workflow.StatefulWorkflow
 import com.squareup.workflow.Workflow
 import com.squareup.workflow.WorkflowHost
-import com.squareup.workflow.WorkflowHost.Factory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.Job
@@ -101,7 +100,7 @@ fun <StateT : Any, OutputT : Any, RenderingT : Any>
 private fun <T, I : Any, O : Any, R : Any> test(
   testBlock: (WorkflowTester<I, O, R>) -> T,
   baseContext: CoroutineContext,
-  starter: (Factory) -> WorkflowHost<I, O, R>
+  starter: (WorkflowHost.Factory) -> WorkflowHost<I, O, R>
 ): T {
   val context = Dispatchers.Unconfined + baseContext + Job(parent = baseContext[Job])
   val host = WorkflowHost.Factory(context)

kotlin/workflow-testing/src/test/java/com/squareup/workflow/ChannelSubscriptionsIntegrationTest.kt
@@ -105,9 +105,7 @@ class ChannelSubscriptionsIntegrationTest {
         assertEquals(1, workflow.subscriptions)
         assertEquals(0, workflow.cancellations)
         setSubscribed(false)
-      }
 
-      host.withNextRendering {
         assertEquals(1, workflow.subscriptions)
         assertEquals(1, workflow.cancellations)
       }
@@ -120,9 +118,7 @@ class ChannelSubscriptionsIntegrationTest {
         assertEquals(1, workflow.subscriptions)
         assertEquals(0, workflow.cancellations)
         setSubscribed(false)
-      }
 
-      host.withNextRendering {
         assertEquals(1, workflow.subscriptions)
         assertEquals(1, workflow.cancellations)
       }