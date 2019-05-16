cf570ca6c41b5bc7d54dfdf78410fa135f704f3d
Dave Apgar <git+apgar@squareup.com>
2019-05-16 16:49:45 -0700

    Fix sample app
    WorkflowTesting shouldn't be linked as a dependency on the SampleApp
    as it relies on XCTest. Adds a test bundle so all the tests can be
    run.

---
+++ swift/Samples/SampleApp/SampleAppTests/SampleAppTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+
+class SampleAppTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
