343b8fd57bc12eed3ff1be67256e66360383b60a
Ray Ryan <ray@squareup.com>
2019-07-19 15:37:06 -0700

    wip

--- kotlin/samples/tictactoe/common/src/test/java/com/squareup/sample/mainworkflow/MainWorkflowTest.kt
+++ kotlin/samples/tictactoe/common/src/test/java/com/squareup/sample/mainworkflow/MainWorkflowTest.kt
@@ -1,6 +1,5 @@
 package com.squareup.sample.mainworkflow
 
-import com.google.common.truth.Truth.assertThat
 import com.squareup.sample.authworkflow.AuthWorkflow
 import com.squareup.sample.authworkflow.Result.Authorized
 import com.squareup.sample.gameworkflow.GamePlayScreen
@@ -15,38 +14,42 @@ import com.squareup.workflow.rendering
 import com.squareup.workflow.stateless
 import com.squareup.workflow.testing.testFromStart
 import com.squareup.workflow.ui.BackStackScreen
-import org.junit.Test
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
 
 /**
  * Demonstrates unit testing of a composite workflow. Note how we
  * pass in fakes for the nested workflows.
  */
-class MainWorkflowTest {
-  @Test fun `starts in auth over empty game`() {
-    MainWorkflow(authWorkflow(), runGameWorkflow()).testFromStart {
-      awaitNextRendering()
-          .let { screen ->
-            assertThat(screen.panels).hasSize(1)
-            assertThat(screen.panels[0]).isEqualTo(DEFAULT_AUTH)
+class MainWorkflowTest : StringSpec() {
+  init {
+    "starts in auth over empty game" {
+      MainWorkflow(authWorkflow(), runGameWorkflow()).testFromStart {
+        awaitNextRendering()
+            .let { screen ->
+              screen.panels shouldHaveSize 1
+              screen.panels[0] shouldBe DEFAULT_AUTH
 
-            // This GamePlayScreen() is emitted by MainWorkflow itself.
-            assertThat(screen.body).isEqualTo(GamePlayScreen())
-          }
-    }
-  }
-
-  @Test fun `starts game on auth`() {
-    val authWorkflow: AuthWorkflow = Workflow.stateless {
-      onWorkerOutput(Worker.from { Unit }) { emitOutput(Authorized("auth")) }
-      authScreen()
+              // This GamePlayScreen() is emitted by MainWorkflow itself.
+              screen.body shouldBe GamePlayScreen()
+            }
+      }
     }
 
-    MainWorkflow(authWorkflow, runGameWorkflow()).testFromStart {
-      awaitNextRendering()
-          .let { screen ->
-            assertThat(screen.panels).isEmpty()
-            assertThat(screen.body).isEqualTo(DEFAULT_RUN_GAME)
-          }
+    "starts game on auth" {
+      val authWorkflow: AuthWorkflow = Workflow.stateless {
+        onWorkerOutput(Worker.from { Unit }) { emitOutput(Authorized("auth")) }
+        authScreen()
+      }
+
+      MainWorkflow(authWorkflow, runGameWorkflow()).testFromStart {
+        awaitNextRendering()
+            .let { screen ->
+              screen.panels shouldHaveSize 0
+              screen.body shouldBe DEFAULT_RUN_GAME
+            }
+      }
     }
   }
 
