327233c7ba0e7ff61f13b0e2e72c7210f1d411ca
Zach Klippenstein <zach.klippenstein@gmail.com>
2019-08-06 13:29:33 -0700

    Empty app for dungeon crawler sample.

---
+++ kotlin/samples/dungeon/app/src/main/java/com/squareup/sample/dungeon/MainActivity.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.sample.dungeon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.workflow.ui.ExperimentalWorkflowUi
+import com.squareup.workflow.ui.WorkflowRunner
+import com.squareup.workflow.ui.setContentWorkflow
+import com.squareup.workflow.ui.workflowOnBackPressed
+
+@UseExperimental(ExperimentalWorkflowUi::class)
+class MainActivity : AppCompatActivity() {
+
+  //  private lateinit var rootWorkflow: DungeonAppWorkflow
+  private lateinit var workflowRunner: WorkflowRunner<*>
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    workflowRunner = setContentWorkflow<Unit>(savedInstanceState) {
+      //      WorkflowRunner.Config(rootWorkflow, viewRegistry)
+      TODO()
+    }
+  }
+
+  override fun onBackPressed() {
+    if (!workflowOnBackPressed()) super.onBackPressed()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    workflowRunner.onSaveInstanceState(outState)
+  }
+
+//  override fun onRetainCustomNonConfigurationInstance(): Any = rootWorkflow
+
+  private companion object {
+//    val viewRegistry = ViewRegistry(TodoEditorLayoutRunner) + TodoListsLayoutRunner
+  }
+}
