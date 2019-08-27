96233f4e894dae63bf0fa2eae5b9c397e5805029
Dave Apgar <apgar@squareup.com>
2019-08-26 18:12:18 -0700

    Add swift tutorial

---
+++ swift/Samples/Tutorial/Frameworks/Tutorial1Complete/Sources/TutorialContainerViewController.swift
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import UIKit
+import Workflow
+import WorkflowUI
+
+
+public final class TutorialContainerViewController: UIViewController {
+    let containerViewController: UIViewController
+
+    public init() {
+        // Create a view registry. This will allow the infrastructure to map `Screen` types to their respective view controller type.
+        var viewRegistry = ViewRegistry()
+        // Register the `WelcomeScreen` and view controller with the convenience method the template provided.
+        viewRegistry.registerWelcomeScreen()
+
+        // Create a `ContainerViewController` with the `WelcomeWorkflow` as the root workflow, with the view registry we just created.
+        containerViewController = ContainerViewController(
+            workflow: WelcomeWorkflow(),
+            viewRegistry: viewRegistry)
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        addChild(containerViewController)
+        view.addSubview(containerViewController.view)
+        containerViewController.didMove(toParent: self)
+    }
+
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        containerViewController.view.frame = view.bounds
+    }
+}
---
+++ swift/Samples/Tutorial/Frameworks/Tutorial4Complete/Sources/Todo/TodoWorkflow.swift
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2019 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import Workflow
+import WorkflowUI
+import BackStackContainer
+import ReactiveSwift
+import Result
+
+
+// MARK: Input and Output
+
+struct TodoWorkflow: Workflow {
+    var name: String
+
+    enum Output {
+        case back
+    }
+}
+
+
+// MARK: State and Initialization
+
+extension TodoWorkflow {
+
+    struct State {
+        var todos: [TodoModel]
+        var step: Step
+        enum Step {
+            // Showing the list of todo items.
+            case list
+            // Editing a single item. The state holds the index so it can be updated when a save action is received.
+            case edit(index: Int)
+        }
+    }
+
+    func makeInitialState() -> TodoWorkflow.State {
+        return State(
+            todos: [TodoModel(
+                title: "Take the cat for a walk",
+                note: "Cats really need their outside sunshine time. Don't forget to walk Charlie. Hamilton is less excited about the prospect.")
+            ],
+            step: .list)
+    }
+
+    func workflowDidChange(from previousWorkflow: TodoWorkflow, state: inout State) {
+
+    }
+}
+
+
+// MARK: Actions
+
+extension TodoWorkflow {
+
+    enum ListAction: WorkflowAction {
+
+        typealias WorkflowType = TodoWorkflow
+
+        case back
+        case editTodo(index: Int)
+        case newTodo
+
+        func apply(toState state: inout TodoWorkflow.State) -> TodoWorkflow.Output? {
+
+            switch self {
+            case .back:
+                return .back
+
+            case .editTodo(index: let index):
+                state.step = .edit(index: index)
+
+            case .newTodo:
+                // Append a new todo model to the end of the list.
+                state.todos.append(TodoModel(
+                    title: "New Todo",
+                    note: ""))
+            }
+
+            return nil
+        }
+    }
+
+    enum EditAction: WorkflowAction {
+
+        typealias WorkflowType = TodoWorkflow
+
+        case discardChanges
+        case saveChanges(index: Int, todo: TodoModel)
+
+        func apply(toState state: inout TodoWorkflow.State) -> TodoWorkflow.Output? {
+            guard case .edit = state.step else {
+                fatalError("Received edit action when state was not `.edit`.")
+            }
+
+            switch self {
+
+            case .discardChanges:
+                state.step = .list
+
+            case .saveChanges(index: let index, todo: let updatedTodo):
+                state.todos[index] = updatedTodo
+
+            }
+            // Return to the list view for either a discard or save action.
+            state.step = .list
+
+            return nil
+        }
+    }
+}
+
+
+// MARK: Workers
+
+extension TodoWorkflow {
+
+    struct TodoWorker: Worker {
+
+        enum Output {
+
+        }
+
+        func run() -> SignalProducer<Output, NoError> {
+            fatalError()
+        }
+
+        func isEquivalent(to otherWorker: TodoWorker) -> Bool {
+            return true
+        }
+
+    }
+
+}
+
+// MARK: Rendering
+
+extension TodoWorkflow {
+
+    typealias Rendering = [BackStackScreen.Item]
+
+    func render(state: TodoWorkflow.State, context: RenderContext<TodoWorkflow>) -> Rendering {
+
+        let todoListItem = TodoListWorkflow(
+            name: name,
+            todos: state.todos)
+            .mapOutput({ output -> ListAction in
+                switch output {
+
+                case .back:
+                    return .back
+
+                case .selectTodo(index: let index):
+                    return .editTodo(index: index)
+
+                case .newTodo:
+                    return .newTodo
+                }
+            })
+            .rendered(with: context)
+
+        switch state.step {
+
+        case .list:
+            // Return only the list item.
+            return [todoListItem]
+
+        case .edit(index: let index):
+
+            let todoEditItem = TodoEditWorkflow(
+                initialTodo: state.todos[index])
+                .mapOutput({ output -> EditAction in
+                    switch output {
+                    case .discard:
+                        return .discardChanges
+
+                    case .save(let updatedTodo):
+                        return .saveChanges(index: index, todo: updatedTodo)
+                    }
+                })
+                .rendered(with: context)
+
+            // Return both the list item and edit.
+            return [todoListItem, todoEditItem]
+        }
+
+    }
+}
