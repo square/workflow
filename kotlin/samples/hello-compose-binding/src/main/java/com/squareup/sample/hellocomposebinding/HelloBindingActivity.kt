df2f94cb462969706c53d7a4c3b331ad96d518c8
Zach Klippenstein <zach.klippenstein@gmail.com>
2019-10-29 10:07:21 -0700

    Basic Jetpack Compose UI ViewBinding integration with sample.
    Co-authored-by: Taras Koshkin <tkoshkin@gmail.com>

---
+++ kotlin/samples/hello-compose-binding/src/main/java/com/squareup/sample/hellocomposebinding/HelloBindingActivity.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2019 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.sample.hellocomposebinding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.workflow.diagnostic.SimpleLoggingDiagnosticListener
+import com.squareup.workflow.ui.ViewRegistry
+import com.squareup.workflow.ui.WorkflowRunner
+import com.squareup.workflow.ui.setContentWorkflow
+
+private val viewRegistry = ViewRegistry(HelloBinding)
+
+class HelloBindingActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentWorkflow(viewRegistry) {
+      WorkflowRunner.Config(
+          HelloWorkflow,
+          diagnosticListener = SimpleLoggingDiagnosticListener()
+      )
+    }
+  }
+}
