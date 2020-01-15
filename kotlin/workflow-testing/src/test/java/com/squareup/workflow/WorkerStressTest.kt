b9effba5571f71a7e2b51b5fbe5db8b92a2fa49b
Zach Klippenstein <zach.klippenstein@gmail.com>
2020-01-15 11:18:49 -0800

    Fix racy broken WorkerStressTests.
    Fixes #878.

--- kotlin/workflow-testing/src/test/java/com/squareup/workflow/WorkerStressTest.kt
+++ kotlin/workflow-testing/src/test/java/com/squareup/workflow/WorkerStressTest.kt
@@ -1,23 +1,25 @@
 package com.squareup.workflow
 
-import com.squareup.workflow.WorkflowAction.Companion.noAction
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.produceIn
 import kotlinx.coroutines.flow.reduce
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class WorkerStressTest {
 
-  @UseExperimental(ExperimentalCoroutinesApi::class)
+  @UseExperimental(ExperimentalCoroutinesApi::class, FlowPreview::class)
   @Test fun `multiple subscriptions to single channel when closed`() {
     val channel = Channel<Unit>()
     val workers = List(100) { channel.asWorker() }
@@ -29,7 +31,9 @@ class WorkerStressTest {
     val workflow = Workflow.stateless<Unit, Unit, Unit> {
       // Run lots of workers that will all see the same close event.
       workers.forEachIndexed { i, worker ->
-        runningWorker(worker, key = i.toString()) { noAction() }
+        runningWorker(worker, key = i.toString()) {
+          fail("Expected non-finishing worker $i not to emit.")
+        }
       }
       finishedWorkers.forEachIndexed { i, worker ->
         runningWorker(worker, key = "finished $i") { action }
@@ -37,15 +41,18 @@ class WorkerStressTest {
     }
 
     runBlocking {
-      val outputs = launchWorkflowIn(this, workflow, flowOf(Unit)) { it.outputs }
+      val outputs = launchWorkflowIn(this, workflow, flowOf(Unit)) {
+        // Collect to a channel immediately so that we don't miss any outputs.
+        it.outputs.produceIn(this)
+      }
 
       // This should just work, and the test will finish, but this is broken by
       // https://github.com/Kotlin/kotlinx.coroutines/issues/1584 and will crash instead if
       // receiveOrClosed is used.
       channel.close()
-      delay(1)
 
-      outputs.take(100)
+      outputs.consumeAsFlow()
+          .take(100)
           .collect()
 
       // Cancel the runtime so the test can finish.
@@ -53,7 +60,7 @@ class WorkerStressTest {
     }
   }
 
-  @UseExperimental(ExperimentalCoroutinesApi::class)
+  @UseExperimental(ExperimentalCoroutinesApi::class, FlowPreview::class)
   @Test fun `multiple subscriptions to single channel when emits`() {
     val channel = ConflatedBroadcastChannel(Unit)
     val workers = List(100) { channel.asWorker() }
@@ -66,8 +73,11 @@ class WorkerStressTest {
     }
 
     runBlocking {
-      val outputs = launchWorkflowIn(this, workflow, flowOf(Unit)) { it.outputs }
-      val sum = outputs.take(100)
+      val outputs = launchWorkflowIn(this, workflow, flowOf(Unit)) {
+        it.outputs.produceIn(this)
+      }
+      val sum = outputs.consumeAsFlow()
+          .take(100)
           .reduce { sum, value -> sum + value }
       assertEquals(100, sum)
 
